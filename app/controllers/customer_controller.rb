bed10e996c6e9973dbf1626a8f9891b222d05eb4
Peter Norcross <pnorcross@bbasolutions.com>
2016-02-20 19:02:12 -0600

    stuff customer

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -2,4 +2,27 @@ class CustomerController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    @customer = Customer.all
+  end
+
+  def new
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    @customer.save
+    redirect_to @customer
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit!
+  end
+
 end
