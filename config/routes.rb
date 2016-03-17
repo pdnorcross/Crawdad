c4b48fd0ee0964a89b108d83b3e9e042248f8ecb
Peter Norcross <pnorcross@bbasolutions.com>
2016-03-17 13:07:38 -0500

    started login in page functionality - Wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-   root 'home#index'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+   root 'sessions#new'
   resources :home
   resources :customers
   resources :reports
