76469496dffaa06ceffe439d99d4b4ee01e17c64
naikilemhsoj <joshmelikian@joshs-mbp.attlocal.net>
2016-04-07 16:47:30 -0500

    init commit - new customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,8 @@
 class Customer < ActiveRecord::Base
 
+has_many :donor 
+accepts_nested_attributes_for :donor
+
   def self.search(search)
     if search
       find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
