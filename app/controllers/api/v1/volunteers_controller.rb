65c4060bb030b1a96f1ce71a2f31db2ed67aee03
Peter Norcross <pnorcross@bbasolutions.com>
2016-04-23 23:30:07 -0500

    added most of method to calculate hours

--- app/controllers/api/v1/volunteers_controller.rb
+++ app/controllers/api/v1/volunteers_controller.rb
@@ -5,14 +5,14 @@ module Api
         def signed_in
           volunteer_value = {}
           volunteer_value['name'] = params(:name)
-          calculated_hours = current_hours
+
 
           volunteers = Volunteer.find_by(name: volunteer_value['name'])
 
           if volunteers.empty?
             volunteers = Volunteer.Create(
                                      name: volunteer_value['name'],
-                                     last_login: Date.now,
+                                     last_login: DateTime.now,
                                      login_status: true
             )
           else
@@ -22,17 +22,20 @@ module Api
                   last_login: Date.now
               )
             else
+              calculated_hours = current_hours(volunteer_value['name'])
               volunteers.update(
                 login_status: false,
-                hours: calculated_hours
+                hours: calculated_hours.to_i
                 )
 
             end
           end
 
           private
-            def
-
+            def current_hours(name)
+              volunteers = Volunteer.find_by(name: name)
+              hours = volunteers.hours + (volunteers.last_login + DateTime.now).to_i
+              return hours
             end
 
 
