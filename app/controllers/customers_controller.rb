f27174d7327731ea27d585c42b1863716675d11f
Peter Norcross <pnorcross@bbasolutions.com>
2016-04-25 21:30:21 -0500

    V 0.1 alpha

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,7 +4,6 @@ class CustomersController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-
     if params[:search]
       @customer = Customer.search(params[:search])
     end
