1946409750681976eb4681653315e98b17c29818
Peter Norcross <pnorcross@bbasolutions.com>
2016-05-09 01:17:27 -0500

    Minor changed to calculated hours

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -14,7 +14,7 @@ class VolunteersController < ApplicationController
 
   def request_sign_in
     volunteers = Volunteer.find_by(name: params[:name])
-    calculated_hours = ((Math.sqrt((volunteers.updated_at - DateTime.now)**2) / 1.hour) + volunteers.hours)
+
 
     if volunteers.nil?
       volunteers = Volunteer.create(
@@ -29,7 +29,7 @@ class VolunteersController < ApplicationController
             login_status: true,
             last_login: DateTime.now )
       else
-        # calculated_hours = current_hours(volunteer_value['name'])
+        calculated_hours = ((Math.sqrt((volunteers.updated_at - DateTime.now)**2) / 1.hour) + volunteers.hours)
         volunteers.update(
             login_status: false,
             hours: calculated_hours )
@@ -38,18 +38,6 @@ class VolunteersController < ApplicationController
     volunteers.save!
     redirect_to :back
   end
-
-
-  # def post_request_sign_in
-  #   require 'mechanize'
-  #   mechanize = Mechanize.new
-  #   post_hash = {
-  #       'name'               => :name,
-  #      }
-  #   mechanize.post('http://localhost:3000/api/v1/volunteers/signed_in', post_hash)
-  #
-  #   redirect_to :new
-  # end
 
 
   private
